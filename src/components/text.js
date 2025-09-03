30a590e95bc811e55b8bc5210b19c901a1a99630
Vayvoda098 <mehmet.e.k098@gmail.com>
2025-09-03 22:46:58 +0300

    Temporarily simplify MovingText for debugging

--- src/components/text.js
+++ src/components/text.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import BlurText from "./BlurText";
+// import BlurText from "./BlurText"; // Temporarily commented out
 
 const MovingText = () => {
-  const handleAnimationComplete = () => {
-    console.log('Animation completed!');
-  };
+  // const handleAnimationComplete = () => {
+  //   console.log('Animation completed!');
+  // };
 
   return (
-    <BlurText
-      text="Isn't this so cool?!"
-      delay={150}
-      animateBy="words"
-      direction="top"
-      onAnimationComplete={handleAnimationComplete}
-      className="text-2xl mb-8"
-    />
+    <div>
+      <h1>Hello from MovingText!</h1>
+      {/* <BlurText
+        text="Isn't this so cool?!"
+        delay={150}
+        animateBy="words"
+        direction="top"
+        onAnimationComplete={handleAnimationComplete}
+        className="text-2xl mb-8"
+      /> */}
+    </div>
   );
 };
 
