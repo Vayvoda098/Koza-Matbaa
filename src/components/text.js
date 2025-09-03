0a6737f4070a96d152dfb611c24cbef80a9cd559
Vayvoda098 <mehmet.e.k098@gmail.com>
2025-09-03 22:49:39 +0300

    Revert MovingText to use BlurText

--- src/components/text.js
+++ src/components/text.js
@@ -1,23 +1,20 @@
 import React from 'react';
-// import BlurText from "./BlurText"; // Temporarily commented out
+import BlurText from "./BlurText";
 
 const MovingText = () => {
-  // const handleAnimationComplete = () => {
-  //   console.log('Animation completed!');
-  // };
+  const handleAnimationComplete = () => {
+    console.log('Animation completed!');
+  };
 
   return (
-    <div>
-      <h1>Hello from MovingText!</h1>
-      {/* <BlurText
-        text="Isn't this so cool?!"
-        delay={150}
-        animateBy="words"
-        direction="top"
-        onAnimationComplete={handleAnimationComplete}
-        className="text-2xl mb-8"
-      /> */}
-    </div>
+    <BlurText
+      text="Isn't this so cool?!"
+      delay={150}
+      animateBy="words"
+      direction="top"
+      onAnimationComplete={handleAnimationComplete}
+      className="text-2xl mb-8"
+    />
   );
 };
 
